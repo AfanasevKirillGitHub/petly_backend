2b79597893ec29b8d8e606448a6ee61e029e41d7
SPoluliakh <S_Poluliakh@ukr.net>
2023-03-30 19:03:22 +0300

    fixed logic

--- controllers/services/getAllServices.js
+++ controllers/services/getAllServices.js
@@ -6,12 +6,19 @@ const getAllServices = async (req, res) => {
 
   const services = await Service.find(
     { [`name.${lang}`]: { $exists: true } },
-    "-createdAt -updatedAt"
+    {
+      [`name.${lang}`]: 1,
+      time: 1,
+      [`address.${lang}`]: 1,
+      email: 1,
+      phone: 1,
+      _id: 0,
+    }
   );
 
-  if (!services) {
+  if (!services.length) {
     throw BadRequest(
-      "Choose another type of language. Availablel options: ua, us."
+      "Choose another type of language. Availablel options: ua, en."
     );
   }
 
