23160ea1da6d64e32f86a92dcf5eb3a7e0c5bc94
i-havr <i.havrylov@gmail.com>
2023-04-09 20:51:56 +0300

    add swagger initial settings

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
@@ -16,6 +18,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/users", usersRouter);
 app.use("/api/services", servicesRouter);
