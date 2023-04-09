dc22f02f49afd3bc4a54ff039b5aada5ad62fe23
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2023-04-09 21:43:11 +0300

    fix/controllers for favorite notices

--- controllers/notices/getFavoriteNotices.js
+++ controllers/notices/getFavoriteNotices.js
@@ -1,10 +1,15 @@
+const { Notice } = require("../../models");
+
 const getFavoriteNotices = async (req, res) => {
     const {_id} = req.user;
     const {page = 1, limit = 20} = req.query;
     const skip = (page - 1) * limit;
-    const pets = await Pet.find({owner: _id}, "", {skip, limit: Number(limit)}).populate("owner", "_id name email");
+    const favoriteNotices = await Notice
+        .find({ owner: _id, favorite }, "", { skip, limit: Number(limit) })
+        .populate("owner", "_id category title name birthdate breed sex location comments price");
+    
     res.status(200).json(
-        pets,
+        favoriteNotices,
     );
 };
 
--- controllers/notices/removeFavoriteNotice.js
+++ controllers/notices/removeFavoriteNotice.js
@@ -1,5 +1,17 @@
-const removeFavoriteNotice = async (req, res) => {
+const { Notice } = require("../../models");
+const HttpError = require("../../helpers");
 
+const removeFavoriteNotice = async (req, res) => {
+    const {noticeId} = req.params;
+    const result = await Notice.findByIdAndRemove(noticeId);
+    
+    if (!result) {
+        throw HttpError(404, "Not found");
+    }
+        
+    res.status(200).json({
+        message: "Notice was removed from favorite category",
+    })
 };
 
 module.exports = removeFavoriteNotice;
