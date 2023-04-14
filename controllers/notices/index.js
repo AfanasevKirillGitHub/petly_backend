b33e20678305d4b264b0d51c59c08eb05583214f
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2023-04-14 23:14:20 +0300

    Update index.js

--- controllers/notices/index.js
+++ controllers/notices/index.js
@@ -3,11 +3,10 @@ const getAllNotices = require("./getAllNotices");
 const getNoticeById = require("./getNoticeById");
 const addNoticeFavorite = require("./addNoticeFavorite");
 const addNotice = require("./addNotice");
-const getFavoriteNotices = require("./getFavoriteNotices");
-const removeFavoriteNotice = require("./removeFavoriteNotice");
-// const addNotice = require("./addNotice.example");
 const getOwnNotices = require("./getOwnNotices");
 const deleteOwnNoticeById = require("./deleteOwnNoticeById");
+const getFavoriteNotices = require("./getFavoriteNotices");
+const removeFavoriteNotice = require("./removeFavoriteNotice");
 
 module.exports = {
   getNoticesByCategory,
@@ -15,9 +14,8 @@ module.exports = {
   getNoticeById,
   addNoticeFavorite,
   addNotice,
-  getFavoriteNotices,
-  removeFavoriteNotice,
-  // addNotice,
   getOwnNotices,
   deleteOwnNoticeById,
+  getFavoriteNotices,
+  removeFavoriteNotice,
 };
