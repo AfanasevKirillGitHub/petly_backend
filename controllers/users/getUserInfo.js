b955c529b07326d8873ddf168b6174ebf444e208
i-havr <i.havrylov@gmail.com>
2023-04-19 13:15:13 +0300

    done swagger docs

--- controllers/users/getUserInfo.js
+++ controllers/users/getUserInfo.js
@@ -2,7 +2,7 @@ const getUserInfo = async (req, res) => {
   const { avatarURL, name, email, birthday, phone, city } = req.user;
 
   res.status(200).json({
-    stauts: "success",
+    status: "success",
     dataUser: {
       avatarURL,
       name,
